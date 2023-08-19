c4142b5b895f49e712930199ea168248dffd3c71
Matthew M-B <matthew.mb@getjobber.com>
2023-08-19 15:06:27 -0400

    Update service file structure

--- service/src/app.ts
+++ service/src/app.ts
@@ -2,12 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import dotenv from "dotenv";
 
-import * as middlewares from './middlewares';
-import api from './api';
-import MessageResponse from './interfaces/MessageResponse';
+import {router, notFound, errorHandler} from './infrastructure/api'
 
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
 
@@ -16,15 +15,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
-});
-
-app.use('/api/v1', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use('/api/v1', router);
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
--- service/src/infrastructure/api/middleware.ts
+++ service/src/infrastructure/api/middleware.ts
@@ -1,15 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 
-import ErrorResponse from './interfaces/ErrorResponse';
-
 export function notFound(req: Request, res: Response, next: NextFunction) {
   res.status(404);
   const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
   next(error);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function errorHandler(err: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction) {
+export function errorHandler(err: Error, _: Request, res: Response, next: NextFunction) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
