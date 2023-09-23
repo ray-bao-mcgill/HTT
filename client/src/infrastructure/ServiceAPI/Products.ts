5df33e5789a6dca5131d50f79031340cd50468a1
Matthew M-B <matthew.mb@getjobber.com>
2023-09-23 15:03:01 -0400

    Update service api format

--- client/src/infrastructure/ServiceAPI/Products.ts
+++ client/src/infrastructure/ServiceAPI/Products.ts
@@ -1,20 +1,25 @@
 export const fetchProducts = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_ROOT}/api/v1/products`);
+  const response = await fetch(
+    `${import.meta.env.VITE_API_ROOT}/api/v1/products`,
+  );
   const json = await response.json();
   return json.data.products;
 };
 
 export const createProduct = async (title: string, description: string) => {
-  const response = await fetch(`${import.meta.env.VITE_API_ROOT}/api/v1/products`, {
-    method: "POST",
-    body: JSON.stringify({
-      title: title,
-      description: description,
-    }),
-    headers: {
-      "Content-Type": "application/json",
+  const response = await fetch(
+    `${import.meta.env.VITE_API_ROOT}/api/v1/products`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        description: description,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     },
-  });
+  );
   const json = await response.json();
   return json.data.product;
 };
