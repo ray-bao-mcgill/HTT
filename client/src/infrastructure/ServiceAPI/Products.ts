f9735b41d67e11f24c77b00d44142dde651bc513
Matthew M-B <matthew.mb@getjobber.com>
2023-09-23 15:02:37 -0400

    Add env variable to client

--- client/src/infrastructure/ServiceAPI/Products.ts
+++ client/src/infrastructure/ServiceAPI/Products.ts
@@ -1,11 +1,11 @@
 export const fetchProducts = async () => {
-  const response = await fetch("http://localhost:5000/api/v1/products");
+  const response = await fetch(`${import.meta.env.VITE_API_ROOT}/api/v1/products`);
   const json = await response.json();
   return json.data.products;
 };
 
 export const createProduct = async (title: string, description: string) => {
-  const response = await fetch("http://localhost:5000/api/v1/products", {
+  const response = await fetch(`${import.meta.env.VITE_API_ROOT}/api/v1/products`, {
     method: "POST",
     body: JSON.stringify({
       title: title,
